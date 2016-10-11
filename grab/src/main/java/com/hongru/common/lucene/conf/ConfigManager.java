1a7ecf7086f4af35a1b76d3647aeda23f653e409
Leo Chen <37799400@qq.com>
2016-10-11 16:16:54 +0800

    修改|加入了indexWriter的commit

--- grab/src/main/java/com/hongru/common/lucene/conf/ConfigManager.java
+++ grab/src/main/java/com/hongru/common/lucene/conf/ConfigManager.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.LogByteSizeMergePolicy;
+import org.apache.lucene.index.LogMergePolicy;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
@@ -53,6 +55,13 @@ public class ConfigManager {
                 if (indexWriter == null) {
                     synchronized (ConfigManager.class) {
                         IndexWriterConfig config = new IndexWriterConfig(analyzer);
+                        LogMergePolicy mergePolicy = new LogByteSizeMergePolicy();
+                        //设置segment添加文档(Document)时的合并频率          //值较小,建立索引的速度就较慢          //值较大,建立索引的速度就较快,>10适合批量建立索引
+                        mergePolicy.setMergeFactor(50);
+                        //设置segment最大合并文档(Document)数
+                        //值较小有利于追加索引的速度
+                        //值较大,适合批量建立索引和更快的搜索
+                        mergePolicy.setMaxMergeDocs(5000);
                         indexWriter = new IndexWriter(getDirectory(), config);
                     }
                 }
